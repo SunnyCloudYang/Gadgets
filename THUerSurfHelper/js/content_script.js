a250f525b85f67207c8d268f9fb01dde29754038
Yunyang <sunnycloudyang@outlook.com>
2023-01-04 16:39:01 +0800

    Banned bing scroll bar and added course regis help

--- THUerSurfHelper/js/content_script.js
+++ THUerSurfHelper/js/content_script.js
@@ -1,13 +1,27 @@
 chrome.storage.sync.get(['autoReload'], ({ autoReload }) => {
+
     if (autoReload) {
+
         let url = window.location.href;
+
         if (url.indexOf("t-hole.github.io") != -1)
             ReloadT_Hole();
+
         else if (url.indexOf("bbs.pku.edu.cn/v2") != -1)
             ReloadPKU();
+
         else if (url.indexOf("planet.tsinghua.edu.cn/member.php") != -1)
             ReloadTHU();
+
+        else if (url == "https://cn.bing.com/") {
+            console.log("THUSurfHelper: Remove scroll bar.");
+            document.getElementsByTagName("html")[0].style.overflowY = "hidden";
+        }
+
+        else if (url.indexOf("https://zhjwxk.cic.tsinghua.edu.cn/xklogin.do") != -1)
+            LoginCRS();
     }
+
 });
 
 function ReloadT_Hole() {
@@ -58,7 +72,9 @@ function Reload() {
 }
 
 function ReloadPKU() {
+
     const dialog_welcome = document.getElementById("dialog-welcome");
+
     if (dialog_welcome) {
         const reload = dialog_welcome.getElementsByClassName("button red")[0];
         if (reload.innerHTML.indexOf("进入未名BBS") != -1)
@@ -70,8 +86,34 @@ function ReloadPKU() {
 
 function ReloadTHU() {
     const message = document.getElementById("messagetext").getElementsByClassName("alert_btnleft")[0];
+    
     if (message.innerHTML.match("如果您的浏览器没有自动跳转，请点击此链接").index != -1)
         message.getElementsByTagName("a")[0].click();
     else
         console.log("Helper: Failed to jump to 1911 planet, please login first or check your Internet.");
+}
+
+function LoginCRS() {
+
+    console.log("THUSurfHelper: Input box helper.")
+
+    const captcha = document.getElementsByName("_login_image_")[0];
+    const username = document.getElementById("j_username");
+    const password = document.getElementById("j_password");
+    const login_btn = document.getElementById("button").getElementsByTagName("a")[0];
+
+    captcha.onkeydown = username.onkeydown = password.onkeydown = function (e) {
+        if (e.key == "Enter") {
+            if (username.value == "")
+                username.focus();
+            else if (password.value == "")
+                password.focus();
+            else if (captcha.value == "")
+                captcha.focus();
+            else {
+                console.log("THUSurfHelper: Login...");
+                login_btn.click();
+            }
+        }
+    }
 }
